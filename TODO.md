b615db7efd3d06ce215a32968772795125327ef9
LucioMansilla <luciomansillaztw@gmail.com>
2023-08-16 02:17:47 -0300

    debug options to build.sh added
    Co-authored-by: BrendaDichiara <BrendaDichiara@users.noreply.github.com>

---
+++ TODO.md
@@ -0,0 +1,8 @@
+[X] - gdb vscode 
+
+[X] - Clang setup
+
+[X] - CMake setup
+
+[X] - CMakeLists.txt: add_executable
+
--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 cd build
 
 # Check if CMakeCache.txt already exists!.
 if [ ! -f CMakeCache.txt ]; then
     cmake ..
+    cmake -DCMAKE_BUILD_TYPE=Debug ..
 fi
-
 cmake --build .
