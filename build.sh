ba565859b7f16ebeab050b83fd655b59b6301d7e
LucioMansilla <luciomansillaztw@gmail.com>
2023-08-16 02:20:31 -0300

    Debug options now its accepted in params
    Co-authored-by: BrendaDichiara <BrendaDichiara@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+
 cd build
 
-# Check if CMakeCache.txt already exists!.
+BUILD_TYPE="Debug"
+
+# Check if an argument was provided
+if [ "$1" != "" ]; then
+    if [ "$1" == "False" ]; then
+        BUILD_TYPE="Release"
+    fi
+fi
+
+# Check if CMakeCache.txt already exists.
 if [ ! -f CMakeCache.txt ]; then
     cmake ..
-    cmake -DCMAKE_BUILD_TYPE=Debug ..
+    cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
 fi
+
 cmake --build .
