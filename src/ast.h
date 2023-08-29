c162cebe54fc8a600c49467c8852ef7f5e26a2cf
LucioMansilla <luciomansillaztw@gmail.com>
2023-08-29 14:14:59 -0300

    ast.h into bison file .y
    Co-authored-by: BrendaDichiara <BrendaDichiara@users.noreply.github.com>

--- src/ast.h
+++ src/ast.h
@@ -6,11 +6,4 @@ typedef struct ASTNode {
     struct ASTNode* right;
 } ASTNode;
 
-typedef struct AST {
-    ASTNode* root;
-} AST;
-
 ASTNode* create_ast_node(Attributes* info, ASTNode* left, ASTNode* right);
-AST* create_ast(ASTNode* root);
-void traverse_ast(AST* ast);
-void destroy_ast(AST* ast);
