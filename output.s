7e6e78eee3e5ab945163c4098edf41c34bf93808
LucioMansilla <luciomansillaztw@gmail.com>
2023-11-19 02:02:31 -0300

    add support for extern function's
    Co-authored-by: BrendaDichiara <BrendaDichiara@users.noreply.github.com>

--- output.s
+++ output.s
@@ -4,26 +4,45 @@
 main:
     pushq %rbp
     movq %rsp, %rbp
-    subq $96, %rsp
-    movq $4, -48(%rbp)
-    movq $0, -64(%rbp)
-    movq -48(%rbp),%rdi 
-    movq $0,%rsi 
-    call print
+    subq $176, %rsp
+    movq $0, -32(%rbp)
+    movq $1, -48(%rbp)
+    movq -32(%rbp), %rax
+    cmpq -48(%rbp), %rax
+    sete %al
+    movzbq %al, %rax
+    movq %rax, -64(%rbp)
+    movq -64(%rbp),%rdi 
+    call print_bool
     movq %rax, -80(%rbp)
-    movq -64(%rbp), %rax
-    cmpq $0, %rax
+    movq -32(%rbp), %rax
+    cmpq $1, %rax
     sete %al
     movzbq %al, %rax
     movq %rax, -96(%rbp)
-    testq %rax, %rax
-    jz LABEL_1
-    movq $10, -48(%rbp)
-LABEL_1:
-    movq -48(%rbp), %rdi
+    movq -96(%rbp),%rdi 
+    call print_bool
+    movq %rax, -112(%rbp)
+    movq $2, %rax
+    cmpq -48(%rbp), %rax
+    sete %al
+    movzbq %al, %rax
+    movq %rax, -128(%rbp)
+    movq -128(%rbp),%rdi 
+    call print_bool
+    movq %rax, -144(%rbp)
+    movq $1, %rax
+    cmpq $2, %rax
+    sete %al
+    movzbq %al, %rax
+    movq %rax, -160(%rbp)
+    movq -160(%rbp),%rdi 
+    call print_bool
+    movq %rax, -176(%rbp)
+    movq $0, %rdi
     movq $0, %rsi
     call print
-    movq  -48(%rbp), %rax
+    movq $0, %rax
     leave
     ret
     .section .note.GNU-stack,"",@progbits
